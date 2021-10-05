c34419c017530e4fcba723ad6a92d236b16471e5
Kovács Krisztián <k.krisztian0830@gmail.com>
2021-10-05 16:20:06 +0200

    Listazas plusz feladat alapján

--- main.js
+++ main.js
@@ -13,18 +13,34 @@ class Film{
 function hozzaadas() {
     let nev = document.getElementById('nev').value;
     let rendezo = document.getElementById('rendezo').value;
-    let kiadaseve = document.getElementById('kiadas').value;
+    let kiadasiev = document.getElementById('kiadasiev').value;
 
-    filmek.push(nev,rendezo,kiadaseve);
+    
+    if (nev != "" && rendezo != "" && kiadasiev != "") {
+        let film=new Film(nev,rendezo,kiadasiev);
+        filmek.push(film);
+    }
+
+    listazas();
 
     document.getElementById('nev').value="";
     document.getElementById('rendezo').value="";
-    document.getElementById('kiadas').value="";
+    document.getElementById('kiadasiev').value="";
 
-    for (let i = 0; i < filmek.length; i++) {
-        console.log(filmek[i]);
-    }
     
+    
+}
+function listazas() {
+    if (filmek.length !== 0) {
+        let a = "<table class='table text-center table-striped'><tr><th>Név</th><th>Rendező</th><th>Kiadási év</th></tr>";
+        for (let i = 0; i < filmek.length; i++) {
+            a += "<tr><td>" + filmek[i].nev + "</td><td>" + filmek[i].rendezo + "</td><td>" + filmek[i].kiadasiev + "</td></tr>";
+        }
+
+        a += "</table>";
+
+        document.getElementById('tablazat').innerHTML = a;
+    }
 }
 
 document.getElementById('hozzaad').addEventListener('click', hozzaadas);
