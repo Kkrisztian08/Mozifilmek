ed8b6cc8a09f8d0c53183f605b4280b260cfb556
Kovács Krisztián <k.krisztian0830@gmail.com>
2021-10-05 15:44:59 +0200

    hozzáad gomb és hozzá tartozó funkció létrehozása

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
+let filmek=[];
+
 class Film{
 
 
     constructor(nev,rendezo,kiadasiev){
         this.nev= nev;
         this.rendezo= rendezo;
-        this.kiadasiev= new Date();
+        this.kiadasiev= kiadasiev;
     }
     
 }
+function hozzaadas() {
+    let nev = document.getElementById('nev').value;
+    let rendezo = document.getElementById('rendezo').value;
+    let kiadaseve = document.getElementById('kiadas').value;
+
+    filmek.push(nev,rendezo,kiadaseve);
+
+    document.getElementById('nev').value="";
+    document.getElementById('rendezo').value="";
+    document.getElementById('kiadas').value="";
+
+    for (let i = 0; i < filmek.length; i++) {
+        console.log(filmek[i]);
+    }
+    
+}
+
+document.getElementById('hozzaad').addEventListener('click', hozzaadas);
